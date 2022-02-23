4605b58769e7ac4af2006063b486c4df16d7918d
Luiz Henrique Alberto de Camargo <71042652+Luiz-Camargo@users.noreply.github.com>
2022-02-23 10:47:58 -0300

    Desafios

---
+++ Quadrado e ao Cubo.cs
@@ -0,0 +1,40 @@
+﻿/*
+Desafio
+Você terá o desafio de escrever um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+
+Entrada
+O arquivo de entrada contém um número inteiro positivo N.
+
+Saída
+Imprima a saída conforme o exemplo fornecido.
+
+Exemplo de Entrada | Exemplo de Saída
+5                  |  1 1 1
+                   |  2 4 8
+                   |  3 9 27
+                   |  4 16 64
+                   |  5 25 125
+______________________________________
+*/
+
+using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int inicio = 1;
+
+            for (int i = 1; i <= n; i++)
+            {    
+                
+                Console.WriteLine($"{i} {(i * i)} {(i * i * i)}");
+            
+            }
+        }
+    }
+}
---
+++ Soma de Pares Consecutivos.cs
@@ -0,0 +1,60 @@
+﻿/*
+Desafio
+O programa deve ler um valor inteiro D indefinidas vezes, ele só irá parar quando o valor de D for igual a 0. Para cada D lido, imprima a soma dos 5 pares consecutivos a partir de D, inclusive ele mesmo , se for par. 
+Se o valor de entrada for 4, por exemplo, a saída deve ser 40, que é o resultado da operação: 4+6+8+10+12, enquanto que se o valor de entrada for 11, por exempo, a saída deve ser 80, que é a soma de 12+14+16+18+20.
+
+Entrada
+O arquivo de entrada contém muitos valores inteiros. O último valor do arquivo é zero.
+
+Saída
+Imprima a saida conforme a explicação acima e o exemplo abaixo.
+
+Exemplo de Entrada | Exemplo de Saída |
+4                  |  40              |
+11                 |  80              | 
+0                  |                  |
+                   |                  |
+______________________________________|
+*/
+
+using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             int contadorY = 1, soma = 0, armazenaX = 0;
+             while(true)
+             {
+               int x = int.Parse(Console.ReadLine());
+               armazenaX = x;
+               
+                if(x % 2 == 0)
+                    soma += x;
+                    
+                else{
+                    armazenaX = x + 1;
+                    soma+= armazenaX;
+                }
+                
+                while(contadorY < 5)
+                {
+                    armazenaX += 2;
+                    soma += armazenaX;
+                    contadorY++;
+                }   
+                
+                if(x == 0)
+                    break;
+
+                Console.WriteLine(soma);
+                contadorY = 1;
+                soma = 0;
+             }
+            
+        }
+    }
+}
